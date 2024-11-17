e687a6f135a2908507bf130062ccc0cf42945bbd
zeriter <791825185@qq.com>
2024-11-17 23:53:54 +0800

    添加基本依赖，设置日志格式

---
+++ openNotes-admin/src/main/java/com/openNotes/RunApp.java
@@ -0,0 +1,17 @@
+package com.openNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZERITER-ZHANG
+ * @version 0.0.1
+ * @description 程序入口
+ * @date 2024/11/17 23:48 周日
+ */
+@SpringBootApplication
+public class RunApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApp.class, args);
+    }
+}
